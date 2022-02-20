4b453122c9853146fdc961a82a38d31fe2bb46c0
Salman <monji@comcast.net>
2022-02-20 15:16:22 -0800

    first commit

---
+++ DentalDDS-API.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DentalDDS_Api;
+using DentalDDS_Api.Controllers;
+
+namespace DentalDDS_Api.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
